6d02cf4714815c02878a69832c648d54431f3c9d
Grégor <gregordrebes@gmail.com>
2021-06-01 00:21:41 -0300

    Ajuste home (controller + view)
    Adicionado pesquisa no topo e exibindo os posts do banco

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\RoleHelper;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +17,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::check()){
+            $is_moderator = Auth::user()->role_id == RoleHelper::MODERATOR;
+        } else {
+            $is_moderator = false;
+        }
+        
+        $posts = $is_moderator ? DB::table('posts')->select("*") : DB::table('posts')->select("*")->whereRaw("deleted = 'f'"); 
+
+        if(!empty($_GET["text"])){
+            $posts->whereRaw("(title like '%{$_GET["text"]}%' or subtitle like '%{$_GET['text']}%' or body like '%{$_GET['text']}%')");
+        }
+        
+        if(!empty($_GET["date1"])){
+            $posts->whereRaw("created_at > '{$_GET["date1"]}'");
+        }
+        
+        if(!empty($_GET["date2"])){
+            // tive que utilizar o where raw ao inves do where
+            $posts->whereRaw("created_at < '{$_GET["date2"]}'");
+            // dd($posts->toSql());
+        }
+
+        // debug
+
+        $posts = $posts->get();
+
+        return view('home', compact("posts"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,85 @@
 @extends('layouts.app')
 
+@push('styles')
+    <style>
+        .posts{
+            transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+        }
+        .posts:hover {
+            transform: scale(1.05);
+            box-shadow: 0 10px 20px rgba(0, 0, 0, .12), 0 4px 8px rgba(0, 0, 0, .06);
+        }
+    </style>
+@endpush
+
+@push('search-bar')
+<div class="col m-2">
+    <button type="button" class="btn btn-primary input-group-append" title="Click here to search" style="cursor: pointer; text-decoration: none !important;" data-toggle="modal" data-target="#searchModal">
+        <span class="pr-2">Search</span>
+        <span class="material-icons m-auto" style="font-size: 18px;">search</span>
+    </button>
+    <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="searchModalLabel">Search filters</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <form id="formModal" class="form-horizontal col" method="get" action="{{ url("/") }}">
+                    @csrf
+                    <fieldset>
+                      <div class="form-group">
+                        <label class="col" for="text">Title, subtitle or body contains</label>
+                        <div class="col">
+                            <input id="text" name="text" type="search"  class="form-control input-md" value="{{ $_GET["text"] ?? "" }}">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="col control-label" for="date1">Created at</label>
+                        <div class="col">
+                            <input id="date1" name="date1" type="date"  class="form-control input-md" value="{{ $_GET["date1"] ?? "" }}">
+                        </div>
+                        <div class="col">
+                          <input id="date2" name="date2" type="date"  class="form-control input-md" value="{{ $_GET["date2"] ?? "" }}">
+                      </div>
+                      </div>
+                    </fieldset>
+                  </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" onclick="window.location='{{ url("/") }}';" class="btn btn-secondary" data-dismiss="modal">Clean search</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" form="formModal" class="btn btn-primary">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+@endpush
+
 @section('content')
     <div class="container">
-        @php
-            $rows = 4;
-            $cols = 3;
-        @endphp
-        @for ($i = 0; $i < $rows; $i++)
-            <div class="row justify-content-center py-2">
-            @for ($j = 0; $j < $cols; $j++)
-                @php
-                    $rand = rand(0, 100) + rand();
-                @endphp
+        <div class="row justify-content-center py-2">
+        @forelse($posts as $p)
                 <div class="col d-flex flex-row justify-content-center mb-3">
                     <div class="card posts" style="width: 18rem;">
-                        <img class="card-img-top" src="https://picsum.photos/200/150?random={{ $rand }}" alt="Card image cap">
+                        <img class="card-img-top" src="{{ $p->thumbnail }}" alt="Thumbnail">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a href="#" class="btn btn-primary btn-red">Go somewhere</a>
+                            <h5 class="card-title">{{ $p->title }}</h5>
+                            <p class="card-text">{{ $p->subtitle }}</p>
+                            <a href="/posts/view/{{$p->slug}}" class="btn btn-primary btn-red">See post</a>
                         </div>
                     </div>
+                    
                 </div>
-            @endfor
-
-            </div>
-        @endfor
-                
+        @empty
+          <div class="col d-flex flex-row justify-content-center">
+            <h3>Nothing here</h3>
+          </div>
+        @endforelse
+        </div>
     </div>
 @endsection
