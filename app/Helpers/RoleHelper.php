41a52b726a894f07133f4362655488421b94b28b
Grégor <gregordrebes@gmail.com>
2021-06-01 00:07:57 -0300

    Add constantes no RoleHelper

--- app/Helpers/RoleHelper.php
+++ app/Helpers/RoleHelper.php
@@ -6,7 +6,10 @@ use App\Role;
 
 class RoleHelper
 {
-
+    public const MODERATOR = 1;
+    public const EDITOR = 2;
+    
+    
     public static function getRoleName($role_id)
     {
         $name = Role::where("id", $role_id)->first()->name;
